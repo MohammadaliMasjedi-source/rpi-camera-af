0a2a683d918cdfba9c9889a3be6d11006035413c
Mohammadali Masjedi <mohammadalimasjedi@gmail.com>
2026-06-06 07:37:46 +0200

    sync: routine commit 2026-06-06 07:37

---
+++ MC-Dashboard.bat
@@ -0,0 +1,3 @@
+@echo off
+REM RPi Camera Autofocus - opens its dashboard (Mission Control, focused on this project)
+start "" "file:///D:/Mo.MassJedi/Git%20Mo/Mission%20Control/index.html#p=rpi-camera-af"
